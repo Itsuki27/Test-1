7d0e379c288b2229f6ba66ff7381b722b3e42aee
Aino <pobletemar6@gmail.com>
2024-11-21 04:12:13 +0800

    AuditLogs
    New Model and Controller

---
+++ WebApplication1/Controllers/MOVEHISTsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MOVEHISTsController : Controller
+    {
+        private AuditLogEntities db = new AuditLogEntities();
+
+        // GET: MOVEHISTs
+        public ActionResult Index()
+        {
+            return View(db.MOVEHISTs.ToList());
+        }
+
+        // GET: MOVEHISTs/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MOVEHIST mOVEHIST = db.MOVEHISTs.Find(id);
+            if (mOVEHIST == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mOVEHIST);
+        }
+
+        // GET: MOVEHISTs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MOVEHISTs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateAuditLogs([Bind(Include = "MOVEHIST_ID,lineno,TYPE,OLD_DATA,NEW_DATA,OLD_SAL,NEW_SAL,D_ACTION,T_ACTION,DESCRIPTION,ACTION_BY,Id")] MOVEHIST mOVEHIST)
+        {
+            bool Status = false;
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                db.MOVEHISTs.Add(mOVEHIST);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            return View(mOVEHIST);
+        }
+
+        // GET: MOVEHISTs/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MOVEHIST mOVEHIST = db.MOVEHISTs.Find(id);
+            if (mOVEHIST == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mOVEHIST);
+        }
+
+        // POST: MOVEHISTs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MOVEHIST_ID,lineno,TYPE,OLD_DATA,NEW_DATA,OLD_SAL,NEW_SAL,D_ACTION,T_ACTION,DESCRIPTION,ACTION_BY,Id")] MOVEHIST mOVEHIST)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mOVEHIST).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mOVEHIST);
+        }
+
+        // GET: MOVEHISTs/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MOVEHIST mOVEHIST = db.MOVEHISTs.Find(id);
+            if (mOVEHIST == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mOVEHIST);
+        }
+
+        // POST: MOVEHISTs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            MOVEHIST mOVEHIST = db.MOVEHISTs.Find(id);
+            db.MOVEHISTs.Remove(mOVEHIST);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Models/Extended/MOVEHIST.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+//namespace WebApplication1.Models.Extended
+
+
+namespace WebApplication1.Models
+{
+    [MetadataType(typeof(AuditLogsData))]
+    public partial class MOVEHIST
+    {
+
+    }
+
+    public class AuditLogsData
+    {
+        public long MOVEHIST_ID { get; set; }
+
+        [Display(Name ="Line Number")]
+        [DataType(DataType.PhoneNumber)]
+        [MinLength(10,ErrorMessage = "Minimum Of 6 Characters Required")]
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Line Number Required")]
+        public long lineno { get; set; }
+
+        [Display(Name = "Type")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Type Required")]
+        public string TYPE { get; set; }
+
+        [Display(Name = "Old Data")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Old Data Requied")]
+        public string OLD_DATA { get; set; }
+
+        [Display(Name = "New Data")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "New Data Required")]
+        public string NEW_DATA { get; set; }
+
+        [Display(Name = "Old Salary")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Old Salary Required")]
+        public string OLD_SAL { get; set; }
+
+        [Display(Name = "New Salary")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "New Salary Required")]
+        public string NEW_SAL { get; set; }
+
+        [Display(Name = "Date Action")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true,DataFormatString ="{0:MM/DD/YYYY}")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Date Action Required")]
+        public string D_ACTION { get; set; }
+
+        [Display(Name = "Time Action")]
+        [DataType(DataType.DateTime)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Time Action Required")]
+        public string T_ACTION { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Description Required")]
+        public string DESCRIPTION { get; set; }
+
+        [Display(Name = "Action By")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Action By Required")]
+        public string ACTION_BY { get; set; }
+
+        [Display(Name = "Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Id Required")]
+        public string Id { get; set; }
+    }
+}
